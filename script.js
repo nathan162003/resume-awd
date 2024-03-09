0fcdfb2f4eec4fcbe47a67446908adc7b4a4b43c
nathan162003 <156502735+nathan162003@users.noreply.github.com>
2024-03-09 15:44:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('changeColor').addEventListener('click', function() {
+    var resume = document.querySelector('.resume');
+    resume.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
